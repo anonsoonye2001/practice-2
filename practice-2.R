c57837668b54a241ffe3af988e4aecbdff4a3f36
anonsoonye2001 <anonsoonye2001@yahoo.co.uk>
2019-09-17 18:50:15 -0500

    edit2

--- practice-2.R
+++ practice-2.R
@@ -2,3 +2,12 @@ God is good
 His kingdom has no end
 
 You are the Lord that never change
+
+x <- data.frame("SN" = 1:2, "Age" = c(21,15), "Name" = c("John","Dora"))
+x
+
+str(x)
+
+m<- matrix(data=1:12, nrow=4,ncol=3,
+           dimnames=list(c("r1","r2","r3","r4"),c("c1","c2","c3")))
+m
